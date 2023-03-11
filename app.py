ba0b272bba73d435b3828be2635216f758883842
Kevin Deakers <kmdeakers@mac.com>
2023-03-10 17:21:02 -0800

    working @app.post(/api/score-word)

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, render_template, jsonify
 from uuid import uuid4
 
+
 from boggle import BoggleGame
+
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "this-is-secret"
@@ -28,3 +31,30 @@ def new_game():
 
     game_data = {"gameId": game_id, "board": game.board}
     return jsonify(game_data)
+
+@app.post("/api/score-word")
+def score_word():
+    """Check if word is valid and score.
+    Accepts JSON of game ID and word"""
+    word = request.json["word"]
+    game_id = request.json["gameId"]
+    print("word", word)
+    #request.json
+    #is_word_in_word_list(word)
+    #check_word_on_board(word)
+    #is_word_not_a_dup(word)
+    #play_and_score_word(word)
+    game = games[game_id]
+    game.is_word_in_word_list(word)
+    game.check_word_on_board(word)
+    game.is_word_not_a_dup(word)
+    game.play_and_score_word(word)
+    
+    if not game.is_word_in_word_list(word):
+        return jsonify(result:"not-word")
+    elif not game.check_word_on_board(word):
+        return jsonify(result:"not-on-board")
+    elif not game.is_word_not_a_dup(word):
+        return jsonify(result:"is-duplicate")
+    else game.play_and_score_word(word):
+        return jsonify(result: )
